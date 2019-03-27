491375a02247d80fe11bd1f44c3c1356a5437523
Claire McCarthy <clmccart@microsoft.com>
2019-03-27 16:58:40 -0700

    flask app

--- Deploy/app.py
+++ Deploy/app.py
@@ -1,12 +1,16 @@
 from flask import Flask
+from flask import jsonify
+from flask import request, make_response
 from score import call_score
+import json
 
 app = Flask(__name__)
-
-@app.route("/")
-def main(data):
+app.debug=True
+@app.route("/", methods=['POST'])
+def main():
+    data = json.loads(request.form['data'], encoding="utf-8")
     results = call_score(data)
-    return results
+    return jsonify(results.tolist())
 
 if __name__ == "__main__":
     app.run()
--- ds_artifacts/score.py
+++ ds_artifacts/score.py
@@ -1,2 +1,11 @@
+import pickle
+import sklearn
+
 def call_score(data):
-    
+    pkl_file = 'fake.pkl'
+    with open(pkl_file, 'rb') as f:
+        model = pickle.load(f)
+
+    # here is where you would mirror pre-processing from when you built the model
+    prediction = model.predict(data)
+    return prediction

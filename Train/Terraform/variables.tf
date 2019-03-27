e2e9ae5fd3b819867a54013ea1dffa256d525bd6
Vaijanath Angadihiremath <vaangadi@VJ-macpro.local>
2019-03-26 22:16:36 -0700

    Initial terraform cluaster commit

---
+++ Train/Terraform/variables.tf
@@ -0,0 +1,34 @@
+variable "resource_group_name" {
+    description = "Existing resource group name to host AKS cluster"
+    default = "aidevopshack"
+}
+
+variable "location" {
+    description = "Location of the resource group"
+    default = "centralus"
+}
+
+variable "vm_size" {
+    description = "Size of the VM"
+    default = "Standard_NC6"
+}
+
+variable "prefix" {
+    description = "String prefix"
+    default = "aidevopshack"
+}
+variable "agent_count" {
+    description = "Number of VM agents nodes."
+    default = "3"
+}
+
+variable "log_analytics_sku" {
+    description = "SKU of the log analytics workspace."
+    default = "PerGB2018"
+}
+
+variable "log_analytics_retention_days" {
+    description = "Log analytics workspace retention days."
+    default = "30"
+}
+

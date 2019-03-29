3478cfbfe6f3f833e2e088b092e6028aef3fd4f3
Vaijanath Angadihiremath <vaangadi@VJ-macpro.local>
2019-03-29 14:04:08 -0700

    setting spid variables

--- Release/Terraform/variables.tf
+++ Release/Terraform/variables.tf
@@ -42,3 +42,19 @@ variable "username" {
   description = "User name"
   default="aidevops"
 }
+
+variable "ARM_CLIENT_ID" {
+  description = "ARM_CLIENT_ID"
+}
+
+variable "ARM_CLIENT_SECRET" {
+    description = "ARM_CLIENT_SECRET"
+}
+
+variable "ARM_SUBSCRIPTION_ID" {
+    description = "ARM_SUBSCRIPTION_ID"
+}
+
+variable "ARM_TENANT_ID" {
+    description = "ARM_TENANT_ID"
+}

95fe950021e5bd6031deac44093077144c84515f
Claire McCarthy <clmccart@microsoft.com>
2019-03-28 17:26:44 -0700

    print exception

--- ds_artifacts/tests.py
+++ ds_artifacts/tests.py
@@ -14,8 +14,8 @@ def test_pkl_file_loads():
     try:
         with open(pkl_file, 'rb') as f:
             model = pickle.load(f)
-    except Exception:
-        pytest.fail(Exception)
+    except Exception as e:
+        pytest.fail(e)
 
 # def test_accuracy_is_greater_than_eighty_percent():
 #     val_data = np.load('ds_artifacts/val_set.npz')

1c23658784bfe02015d60ada08db8d8b73977db5
Vaijanath Angadihiremath <vaangadi@VJ-macpro.local>
2019-03-29 14:53:30 -0700

    test

--- Release/Terraform/main.tf
+++ Release/Terraform/main.tf
@@ -6,7 +6,7 @@ provider "azuread" {
   # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
  
   subscription_id = "${var.ARM_SUBSCRIPTION_ID}"
-  client_id       = "${var.ARM_CLIENT_ID}"
+  client_id       = "${var.client_id}"
   client_secret   = "${var.ARM_CLIENT_SECRET}"
   tenant_id       = "${var.ARM_TENANT_ID}"
 }
@@ -15,7 +15,7 @@ provider "azurerm" {
   # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
  
   subscription_id = "${var.ARM_SUBSCRIPTION_ID}"
-  client_id       = "${var.ARM_CLIENT_ID}"
+  client_id       = "${var.client_id}"
   client_secret   = "${var.ARM_CLIENT_SECRET}"
   tenant_id       = "${var.ARM_TENANT_ID}"
 }
